91b2c6281793a50fbde940333b01fc5e376c5d05
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-03-25 07:44:37 +0530

    Add files via upload

---
+++ Bubblesort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class Bubblesort {
+    public static void bubblesort(int[]arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        int arr[]={70,80,30,40,50};
+        bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
